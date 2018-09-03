c924339e6a622065799778ed25cf2a27690ef027
Edu Alcover <alcover.edu@gmail.com>
2018-09-03 12:04:46 +0200

    controller, loginController

---
+++ controllers/loginController.js
@@ -0,0 +1,9 @@
+import Controller from './controller';
+
+class LoginController extends Controller{
+    constructor(req, res, next){
+        super(req, res, next);
+    }
+}
+
+module.exports = LoginController;
